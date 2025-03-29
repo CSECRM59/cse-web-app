2f80ac410260d76ff3ae14b9b6eceafa500bac78
CSECRM59 <csecrm59300@gmail.com>
2025-03-29 17:14:16 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 
-//import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 
